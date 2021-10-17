5106d0f039e1019e62aa803bd5c34b98d569860e
KrysP <KrysP@LAPTOP-FFF4P298>
2021-10-17 20:47:16 +0100

    restructured the entire file layout and cleaned stuff up

---
+++ source/scripts/theme_mgr.js
@@ -0,0 +1,28 @@
+var icon = document.getElementById("icon");
+
+if(localStorage.getItem("theme") == null){
+    localStorage.setItem("theme", "light");
+}
+
+let theme = localStorage.getItem("theme");
+
+if (theme == "dark") {
+    document.body.classList.toggle("dark-theme");
+        icon.src = "images/icons/sun.png";
+}
+
+icon.onclick = function() {
+
+    document.body.classList.toggle("dark-theme");
+
+    if(document.body.classList.contains("dark-theme")){
+
+        localStorage.setItem("theme", "dark");
+        icon.src = "images/icons/sun.png";
+
+    } else {
+        
+        localStorage.setItem("theme", "light");
+        icon.src = "images/icons/moon.png";
+    }
+}
